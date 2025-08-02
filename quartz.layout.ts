e91847b03233d09e6ff661bed02e0431ae99cfd9
Darius Bittle-Dockery <dbd@Bitbook.local>
2025-08-02 10:18:51 -0400

    Quartz sync: Aug 2, 2025, 10:18 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "GreyMatters Marketing": "https://graymattersmarketing.com/",
+      "AARP Homepage": "https://www.aarp.org",
     },
   }),
 }
@@ -71,7 +71,7 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [Component.ArticleTitle(), Component.ContentMeta()],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
